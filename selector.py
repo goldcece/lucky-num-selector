83d12be183a10ede0d08b1ec2a50bf3295dedcff
goldcece <jeacece@gmail.com>
2023-05-01 14:35:18 +0900

    feat : Edit selector

--- selector.py
+++ selector.py
@@ -3,12 +3,15 @@
 
 import random
 
-def random_select():
+#def random_select():
 
-    num_list = []
-    for i in range(6):
-	n = random.randint(1,46)
-	num_list.append(n)
+#    num_list = []
+#    for i in range(6):
+#	n = random.randint(1,46)
+#	num_list.append(n)
 
-print(num_list)
-	
+# print(num_list)
+
+
+result = random.sample(range(1, 45+1), k=6)
+print(result)	
